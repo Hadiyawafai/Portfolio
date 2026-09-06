5b7b21a4329249240057fe9a134b6843f92549e0
hadiya wafai <hadiyawafai@gmail.com>
2026-09-06 13:21:55 +0530

    Fix Swagger package version

--- Backend/Portfolio/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/Portfolio/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f3cf1172f7ec61f5a3e463408443bc24d49b14c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70639159eeec528d6327127bc7e9301d82782f53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
