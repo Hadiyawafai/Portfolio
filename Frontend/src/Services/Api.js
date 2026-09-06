3334ae038471708fbc2df618d69f03823bf66173
hadiya wafai <hadiyawafai@gmail.com>
2026-09-06 11:18:20 +0530

    fixed part

--- Frontend/src/Services/Api.js
+++ Frontend/src/Services/Api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-const api=axios.create({
-    baseURL:"http://localhost:5299/api",
-    headers:{
-        "Content-Type":"application/json"
-    }
-})
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 export default api;
