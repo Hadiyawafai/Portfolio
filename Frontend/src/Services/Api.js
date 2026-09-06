25aa3ed978f146ce43b9267ed0e275f9deff1080
hadiya wafai <hadiyawafai@gmail.com>
2026-09-06 15:28:51 +0530

    Fix production API URL

--- Frontend/src/Services/Api.js
+++ Frontend/src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://portfolio-5-wz74.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
